3a9548c3023756f9b2f84970814ba14249ae9393
Charles Jekel <cjekel@gmail.com>
2019-01-22 11:07:28 -0500

    change cores to 4

--- lin_ortho_known/write_known_results.py
+++ lin_ortho_known/write_known_results.py
@@ -36,7 +36,7 @@ def write_material_model(x):
 
 
 def run_model():
-    abqcommand = 'abq job=model interactive cpus=1 ask_delete=OFF'
+    abqcommand = 'abq job=model interactive cpus=4 ask_delete=OFF'
     val = os.system(abqcommand)
     # on linux val == 0 when success
     return val
