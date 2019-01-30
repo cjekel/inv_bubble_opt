63fcfc1838720caa81780abcb0adc40283fbdc80
Charles Jekel <cjekel@gmail.com>
2019-01-30 17:11:34 -0500

    re run optimizations that failed

--- lin_ortho_known/optimization_blue_cv01.py
+++ lin_ortho_known/optimization_blue_cv01.py
@@ -30,46 +30,46 @@ if __name__ == "__main__":
     invbubble.delete_files()
 
     # load the test data
-    # blue00 = np.load(expanduser('~/temp/testdata/blue00.npy'))
-    blue01 = np.load(expanduser('~/temp/testdata/blue01.npy'))
-    blue02 = np.load(expanduser('~/temp/testdata/blue02.npy'))
-    blue03 = np.load(expanduser('~/temp/testdata/blue03.npy'))
+    # blue00 = np.load(expanduser('testdata/blue00.npy'))
+    blue01 = np.load(expanduser('testdata/blue01.npy'))
+    blue02 = np.load(expanduser('testdata/blue02.npy'))
+    blue03 = np.load(expanduser('testdata/blue03.npy'))
     test_data = [blue01, blue02, blue03]
 
     # initialize a maximum objective value
     max_obj = 30.0  # mm
 
-    opt_hist_file = '~/my_blue_history_cv01.csv'
+    opt_hist_file = 'my_blue_history_cv01.csv'
     header = ['E1', 'E2', 'G12', 'OBJ', 'Success']
     my_opt = invbubble.BubbleOpt(opt_hist_file, header, max_obj,
                                  'xy_model.npy', 'disp_values.npy',
                                  test_data=test_data)
 
-    def conv_my_obj(x):
-        f = np.zeros(x.shape[0])
-        for i, j in enumerate(x):
-            f[i] = my_opt.calc_obj_function_test_data(j)
-        return f
+    # def conv_my_obj(x):
+    #     f = np.zeros(x.shape[0])
+    #     for i, j in enumerate(x):
+    #         f[i] = my_opt.calc_obj_function_test_data(j)
+    #     return f
 
-    bounds = [{'name': 'var_1', 'type': 'continuous', 'domain': [0.1, 2.0]},
-              {'name': 'var_2', 'type': 'continuous', 'domain': [0.05, 1.0]},
-              {'name': 'var_3', 'type': 'continuous', 'domain': [0.001, 0.2]}]
+    # bounds = [{'name': 'var_1', 'type': 'continuous', 'domain': [0.1, 2.0]},
+    #           {'name': 'var_2', 'type': 'continuous', 'domain': [0.05, 1.0]},
+    #           {'name': 'var_3', 'type': 'continuous', 'domain': [0.001, 0.2]}]
 
-    max_iter = 30
-    np.random.seed(121)
-    myBopt = BayesianOptimization(conv_my_obj, domain=bounds, model_type='GP',
-                                  initial_design_numdata=20,
-                                  initial_design_type='latin',
-                                  exact_feval=True, verbosity=True,
-                                  verbosity_model=False)
+    # max_iter = 30
+    # np.random.seed(121)
+    # myBopt = BayesianOptimization(conv_my_obj, domain=bounds, model_type='GP',
+    #                               initial_design_numdata=20,
+    #                               initial_design_type='latin',
+    #                               exact_feval=True, verbosity=True,
+    #                               verbosity_model=False)
 
-    myBopt.run_optimization(max_iter=max_iter, eps=1e-7, verbosity=True,
-                            report_file='gp_opt_results')
+    # myBopt.run_optimization(max_iter=max_iter, eps=1e-7, verbosity=True,
+    #                         report_file='gp_opt_results')
 
-    print('\n \n EGO Opt Complete \n')
-    print('X values:', myBopt.x_opt)
-    print('Function value:', myBopt.fx_opt)
-
+    # print('\n \n EGO Opt Complete \n')
+    # print('X values:', myBopt.x_opt)
+    # print('Function value:', myBopt.fx_opt)
+    x0 = [0.34069393, 0.17930365, 0.00824243]
     my_bounds = np.zeros((3, 2))
     my_bounds[0, 0] = 0.1
     my_bounds[0, 1] = 2.0
@@ -78,8 +78,8 @@ if __name__ == "__main__":
     my_bounds[2, 0] = 0.001
     my_bounds[2, 1] = 0.2
     x0 = [0.26148553, 0.24668236, 0.01]
-    res = fmin_l_bfgs_b(my_opt.calc_obj_function_test_data, myBopt.x_opt,
+    res = fmin_l_bfgs_b(my_opt.calc_obj_function_test_data, x0,
                         approx_grad=True, bounds=my_bounds, factr=1e7,
                         pgtol=1e-06, epsilon=1e-4, iprint=1,
-                        maxfun=200, maxiter=120, maxls=25)
+                        maxfun=200, maxiter=120, maxls=20)
     print(res)
