775ec94bf1ebcb4d3514a0dc19ddb0d6106b3ae8
Charles Jekel <cjekel@gmail.com>
2019-02-05 10:39:08 -0500

    updat cv04

--- lin_ortho_known/optimization_blue_cv04.py
+++ lin_ortho_known/optimization_blue_cv04.py
@@ -39,7 +39,7 @@ if __name__ == "__main__":
     # initialize a maximum objective value
     max_obj = 30.0  # mm
 
-    opt_hist_file = 'my_blue_history_cv04.csv'
+    opt_hist_file = 'my_blue_history_cv04r1.csv'
     header = ['E1', 'E2', 'G12', 'OBJ', 'Success']
     my_opt = invbubble.BubbleOpt(opt_hist_file, header, max_obj,
                                  'xy_model.npy', 'disp_values.npy',
@@ -51,24 +51,24 @@ if __name__ == "__main__":
             f[i] = my_opt.calc_obj_function_test_data(j)
         return f
 
-    bounds = [{'name': 'var_1', 'type': 'continuous', 'domain': [0.1, 2.0]},
-              {'name': 'var_2', 'type': 'continuous', 'domain': [0.05, 1.0]},
-              {'name': 'var_3', 'type': 'continuous', 'domain': [0.001, 0.2]}]
+    # bounds = [{'name': 'var_1', 'type': 'continuous', 'domain': [0.1, 2.0]},
+    #           {'name': 'var_2', 'type': 'continuous', 'domain': [0.05, 1.0]},
+    #           {'name': 'var_3', 'type': 'continuous', 'domain': [0.001, 0.2]}]
 
-    max_iter = 30
-    np.random.seed(121)
-    myBopt = BayesianOptimization(conv_my_obj, domain=bounds, model_type='GP',
-                                  initial_design_numdata=20,
-                                  initial_design_type='latin',
-                                  exact_feval=True, verbosity=True,
-                                  verbosity_model=False)
+    # max_iter = 30
+    # np.random.seed(121)
+    # myBopt = BayesianOptimization(conv_my_obj, domain=bounds, model_type='GP',
+    #                               initial_design_numdata=20,
+    #                               initial_design_type='latin',
+    #                               exact_feval=True, verbosity=True,
+    #                               verbosity_model=False)
 
-    myBopt.run_optimization(max_iter=max_iter, eps=1e-7, verbosity=True,
-                            report_file='gp_opt_results')
+    # myBopt.run_optimization(max_iter=max_iter, eps=1e-7, verbosity=True,
+    #                         report_file='gp_opt_results')
 
-    print('\n \n EGO Opt Complete \n')
-    print('X values:', myBopt.x_opt)
-    print('Function value:', myBopt.fx_opt)
+    # print('\n \n EGO Opt Complete \n')
+    # print('X values:', myBopt.x_opt)
+    # print('Function value:', myBopt.fx_opt)
 
     my_bounds = np.zeros((3, 2))
     my_bounds[0, 0] = 0.1
@@ -77,9 +77,10 @@ if __name__ == "__main__":
     my_bounds[1, 1] = 1.0
     my_bounds[2, 0] = 0.001
     my_bounds[2, 1] = 0.2
-    x0 = [0.26148553, 0.24668236, 0.01]
-    res = fmin_l_bfgs_b(my_opt.calc_obj_function_test_data, myBopt.x_opt,
-                        approx_grad=True, bounds=my_bounds, factr=1e7,
-                        pgtol=1e-06, epsilon=1e-4, iprint=1,
-                        maxfun=200, maxiter=120, maxls=25)
+    my_bounds[2, :] *= 100.0
+    x0 = [0.27982852, 0.2445296 , 0.00321272*100]
+    res = fmin_l_bfgs_b(my_opt.calc_obj_function_test_data, x0,
+                        approx_grad=True, bounds=my_bounds, factr=10,
+                        pgtol=1e-06, epsilon=1e-3, iprint=1,
+                        maxfun=200, maxiter=5, maxls=25)
     print(res)
