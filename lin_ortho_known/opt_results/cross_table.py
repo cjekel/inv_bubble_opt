270194ad51d63b5095081d4762d09a700908ef11
cjekel <cjekel@gmail.com>
2019-02-21 14:32:49 -0500

    add cv score calc

--- lin_ortho_known/opt_results/cross_table.py
+++ lin_ortho_known/opt_results/cross_table.py
@@ -7,3 +7,14 @@ header = ['Full OBJ', 'CV1 OBJ', 'CV2 OBJ', 'CV3 OBJ', 'CV4 OBJ']
 header2 = ['Full mat', 'CV1 mat', 'CV2 mat', 'CV3 mat', 'CV4 mat']
 
 mydf = pd.DataFrame(res, index=header2, columns=header)
+
+res2 = np.load('blue_cross_compute_sep.npy')
+
+header = ['CV1 OBJ', 'CV2 OBJ', 'CV3 OBJ', 'CV4 OBJ']
+header2 = ['Full mat', 'CV1 mat', 'CV2 mat', 'CV3 mat', 'CV4 mat']
+
+mysep = pd.DataFrame(res2, index=header2, columns=header)
+
+mycv = np.diagonal(mysep.values[1:])
+print('cv mean', mycv.mean())
+print('cv std', mycv.std(ddof=1))
