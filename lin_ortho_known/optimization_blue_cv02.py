44b1dfca5d012fc14df16000a8a0f41fce152de1
Charles Jekel <cjekel@gmail.com>
2019-07-08 14:02:58 -0400

    allow_pickle=True commit 00

--- lin_ortho_known/optimization_blue_cv02.py
+++ lin_ortho_known/optimization_blue_cv02.py
@@ -31,10 +31,10 @@ if __name__ == "__main__":
 
     # load the test data
     homeuser = os.path.expanduser('~')
-    blue00 = np.load(os.path.join(homeuser, 'blue00.npy'))
-    blue01 = np.load(os.path.join(homeuser, 'blue01.npy'))
-    blue02 = np.load(os.path.join(homeuser, 'blue02.npy'))
-    blue03 = np.load(os.path.join(homeuser, 'blue03.npy'))
+    blue00 = np.load(os.path.join(homeuser, 'blue00.npy'), allow_pickle=True)
+    blue01 = np.load(os.path.join(homeuser, 'blue01.npy'), allow_pickle=True)
+    blue02 = np.load(os.path.join(homeuser, 'blue02.npy'), allow_pickle=True)
+    blue03 = np.load(os.path.join(homeuser, 'blue03.npy'), allow_pickle=True)
     test_data = [blue00, blue02, blue03]
 
     # initialize a maximum objective value
