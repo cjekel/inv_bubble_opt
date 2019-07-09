b5374352497d0553bc5240d5e0d48b0e2da17af0
cjekel <cjekel@gmail.com>
2019-07-09 16:54:23 -0400

    change backend default

--- invbubble_py_source/invbubble/invbubble.py
+++ invbubble_py_source/invbubble/invbubble.py
@@ -187,40 +187,6 @@ class InterpolateThenRBF(object):
             dz_delta[i] = np.nanmean(np.abs(dz_new - Disp_new[i, :, 2]))
         return dx_delta, dy_delta, dz_delta
 
-    def calc_delta_test_par(self, i):
-        p = self.Ps[i]
-        newX = self.X_new[i][:, :2]
-        Disp_new = self.X_new[i][:, 3:]
-        dx_new, dy_new, dz_new = self.calc_disp(newX, p)
-        dx_delta = np.nanmean(np.abs(dx_new - Disp_new[:, 0]))
-        dy_delta = np.nanmean(np.abs(dy_new - Disp_new[:, 1]))
-        dz_delta = np.nanmean(np.abs(dz_new - Disp_new[:, 2]))
-        return dx_delta, dy_delta, dz_delta
-
-    def calc_delta_test(self, X_new, Ps):
-        # for test data from bubble test
-        # calculate the average deviation for each p in Ps
-        np = len(Ps)
-        dx_delta = np.zeros(np)
-        dy_delta = np.zeros(np)
-        dz_delta = np.zeros(np)
-        self.X_new = X_new
-        self.Ps = Ps
-        # for i, p in enumerate(Ps):
-        #     newX = X_new[i][:, :2]
-        #     Disp_new = X_new[i][:, 3:]
-        #     dx_new, dy_new, dz_new = self.calc_disp(newX, p)
-        #     dx_delta[i] = np.nanmean(np.abs(dx_new - Disp_new[:, 0]))
-        #     dy_delta[i] = np.nanmean(np.abs(dy_new - Disp_new[:, 1]))
-        #     dz_delta[i] = np.nanmean(np.abs(dz_new - Disp_new[:, 2]))
-        dx = Parallel(n_jobs=-1)(delayed(self.calc_delta_test_par)(i) for i in range(np))  # noqa E501
-        dx = np.array(dx)
-        print(dx.shape)
-        dx_delta = dx[:, 0]
-        dy_delta = dx[:, 1]
-        dz_delta = dx[:, 2]
-        return dx_delta, dy_delta, dz_delta
-
 
 def rbf_function(xi, di, xih):
     "simple rbf linear function inspired by scipy.interploate.rbf"
@@ -271,32 +237,38 @@ class InterpolateSimpleRBF(object):
             dz = rbf_function(self.xy, ind[:, 2], Xnew[:, :2])
         return dx, dy, dz
 
-    def calc_delta(self, X_new, Ps, Disp_new):
-        # for numerical model
-        # calculate the average deviation for each p in Ps
-        dx_delta = np.zeros(len(Ps))
-        dy_delta = np.zeros(len(Ps))
-        dz_delta = np.zeros(len(Ps))
-        for i, p in enumerate(Ps):
-            dx_new, dy_new, dz_new = self.calc_disp(X_new[i], p)
-            dx_delta[i] = np.nanmean(np.abs(dx_new - Disp_new[i, :, 0]))
-            dy_delta[i] = np.nanmean(np.abs(dy_new - Disp_new[i, :, 1]))
-            dz_delta[i] = np.nanmean(np.abs(dz_new - Disp_new[i, :, 2]))
+    def calc_delta_test_par(self, i):
+        p = self.Ps[i]
+        newX = self.X_new[i][:, :2]
+        Disp_new = self.X_new[i][:, 3:]
+        dx_new, dy_new, dz_new = self.calc_disp(newX, p)
+        dx_delta = np.nanmean(np.abs(dx_new - Disp_new[:, 0]))
+        dy_delta = np.nanmean(np.abs(dy_new - Disp_new[:, 1]))
+        dz_delta = np.nanmean(np.abs(dz_new - Disp_new[:, 2]))
         return dx_delta, dy_delta, dz_delta
 
     def calc_delta_test(self, X_new, Ps):
         # for test data from bubble test
         # calculate the average deviation for each p in Ps
-        dx_delta = np.zeros(len(Ps))
-        dy_delta = np.zeros(len(Ps))
-        dz_delta = np.zeros(len(Ps))
-        for i, p in enumerate(Ps):
-            newX = X_new[i][:, :2]
-            Disp_new = X_new[i][:, 3:]
-            dx_new, dy_new, dz_new = self.calc_disp(newX, p)
-            dx_delta[i] = np.nanmean(np.abs(dx_new - Disp_new[:, 0]))
-            dy_delta[i] = np.nanmean(np.abs(dy_new - Disp_new[:, 1]))
-            dz_delta[i] = np.nanmean(np.abs(dz_new - Disp_new[:, 2]))
+        np = len(Ps)
+        dx_delta = np.zeros(np)
+        dy_delta = np.zeros(np)
+        dz_delta = np.zeros(np)
+        self.X_new = X_new
+        self.Ps = Ps
+        # for i, p in enumerate(Ps):
+        #     newX = X_new[i][:, :2]
+        #     Disp_new = X_new[i][:, 3:]
+        #     dx_new, dy_new, dz_new = self.calc_disp(newX, p)
+        #     dx_delta[i] = np.nanmean(np.abs(dx_new - Disp_new[:, 0]))
+        #     dy_delta[i] = np.nanmean(np.abs(dy_new - Disp_new[:, 1]))
+        #     dz_delta[i] = np.nanmean(np.abs(dz_new - Disp_new[:, 2]))
+        dx = Parallel(n_jobs=-1, backend='loky')(delayed(self.calc_delta_test_par)(i) for i in range(np))  # noqa E501
+        dx = np.array(dx)
+        print(dx.shape)
+        dx_delta = dx[:, 0]
+        dy_delta = dx[:, 1]
+        dz_delta = dx[:, 2]
         return dx_delta, dy_delta, dz_delta
 
 
