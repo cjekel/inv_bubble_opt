127106c2e328f732fa819cea5ffc2ccb7b6faed5
Charles Jekel <cjekel@gmail.com>
2019-07-10 13:00:22 -0400

    reweight again

--- lin_ortho_known/re_weight_03.py
+++ lin_ortho_known/re_weight_03.py
@@ -45,7 +45,7 @@ if __name__ == "__main__":
     # initialize a maximum objective value
     max_obj = 30.0  # mm
 
-    opt_hist_file = 'weight03r02.csv'
+    opt_hist_file = 'weight03r03.csv'
     header = ['E1', 'E2', 'G12', 'OBJ', 'Success']
     my_opt = invbubble.BubbleOpt(opt_hist_file, header, max_obj,
                                  None, None,
