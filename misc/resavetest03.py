9d4d712678e005aad905a1abe1f30bf83f7d1c4c
cjekel <cjekel@gmail.com>
2019-08-06 07:40:48 -0400

    resize

--- misc/resavetest03.py
+++ misc/resavetest03.py
@@ -3,5 +3,5 @@ import os
 homeuser = os.path.expanduser('~')
 
 blue03 = np.load(os.path.join(homeuser, 'blue03.npy'), allow_pickle=True)
-# blue03 = blue03[18:]
-# np.save('blue03.npy', blue03)
+blue03 = blue03[18:]
+np.save('blue03.npy', blue03)
