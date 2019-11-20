47d05cc4ee7c9d2fac1e093893b39a6ea0d6c786
cjekel <cjekel@gmail.com>
2019-11-20 09:13:03 -0800

    update runfile

--- lin_ortho_gen_data/save_compressed_version.py
+++ lin_ortho_gen_data/save_compressed_version.py
@@ -4,7 +4,7 @@ import h5py
 from time import time
 
 start_point = 0
-end_point = 10
+end_point = 2000
 
 
 def read_csv_files(i):
@@ -26,11 +26,11 @@ def read_csv_files(i):
 
 data = np.zeros((end_point-start_point, 199, 937, 3), dtype=np.single)
 
-for i in range(start_point, end_point+1):
+for i in range(start_point, end_point):
     t0 = time()
     data[i] = read_csv_files(i)
     t1 = time()
     print('Runtime:', t1-t0)
 
 with h5py.File('data2k.hdf5', 'w') as f:
-    dset = f.create_dataset('data2k', data, compression="gzip")
+    dset = f.create_dataset('data2k', data=data, compression="gzip")
