729a5aa8f114ef93bc3079f679d65658c72bf0fd
cjekel <cjekel@gmail.com>
2019-11-24 15:05:49 -0800

    add success count

--- lin_ortho_gen_data/generate_data_surrogate.py
+++ lin_ortho_gen_data/generate_data_surrogate.py
@@ -28,12 +28,14 @@ def run_abq_model(x):
 for i in range(start_point, end_point+1):
     t0 = time()
     val, suc = run_abq_model(Y[i])
+    suc_count = 0
     if suc is True:
         if val == 0:
             val2 = invbubble.export_csv_files()
             if val2 == 0:
                 # copy files to new dir
                 copytree('BubbleTest', 'BubbleTest' + str(i).zfill(5))
+                suc_count += 1
     invbubble.delete_files()
     t1 = time()
-    print('Runetime:', t1 - t0, ' i:', i)
+    print('Runetime:', t1 - t0, ' i:', i, 'Success count:', suc_count)
