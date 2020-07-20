51fcc598e46350079ff0a8a450de51687cee52f5
Charles Jekel <cjekel@gmail.com>
2020-07-19 17:32:56 -0700

    add z only data

--- misc/get_max_regions_of_data/save_z_max_loc_of_test_data.py
+++ misc/get_max_regions_of_data/save_z_max_loc_of_test_data.py
@@ -22,32 +22,17 @@ max_dz = np.array([[0.0, 0.0]])
 for i in range(4):
     dataset = np.load(os.path.join(homeuser, datanames[i]), allow_pickle=True)
     new_dataset = []
-    xyz_dataset = np.empty((3, 6))
     for j in range(len(dataset)):
-        xyz_dataset.fill(np.nan)
+        xyz_dataset = np.zeros((1, 6))
         p = dataset[j, 1]
         # find the data points closes to the max_x, max_y, and max_z
         # deflection locations
         xy = dataset[j, 0][:, :2]
-        d = cdist(xy, max_dx)
-        # set the xy location and grab the displacement value
-        # dx
-        d_ind = np.argmin(d)
-        xyz_dataset[0, :4] = dataset[j, 0][d_ind, :4]
 
-        # set the xy location and grab the displacement value
-        # dy
-        d = cdist(xy, max_dy)
-        d_ind = np.argmin(d)
-        xyz_dataset[1, :3] = dataset[j, 0][d_ind, :3]
-        xyz_dataset[1, 4] = dataset[j, 0][d_ind, 4]
-
-        # set the xy location and grab the displacement value
-        # dz
         d = cdist(xy, max_dz)
         d_ind = np.argmin(d)
-        xyz_dataset[2, :3] = dataset[j, 0][d_ind, :3]
-        xyz_dataset[2, 5] = dataset[j, 0][d_ind, 5]
+        xyz_dataset[0, :3] = dataset[j, 0][d_ind, :3]
+        xyz_dataset[0, 5] = dataset[j, 0][d_ind, 5]
         new_dataset.append([xyz_dataset, p])
 
     np.save('z_max_only_' + datanames[i], new_dataset)
